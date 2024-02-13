84665a0f10abe73368f05486041aff9910406d26
Aryan Padmakar Shirsat <aryanshirsat357@gmail.com>
2024-02-13 18:34:39 +0530

    restored Sponsors page

--- app/sponsors/page.js
+++ app/sponsors/page.js
@@ -3,7 +3,7 @@ import { sponsors } from "@/constants";
 import Navbar from "@/components/Navbar";
 const Sponsorcard = ({ logo, name, intro, link }) => {
   return (
-    <div className="w-fit h-max relative flex items-center justify-center before:w-[100px] before:h-[100px] before:content-[''] before:absolute before:bg-[rgb(144,161,255)] before:z-[-1] before:transition-all before:duration-[1s] before:rounded-[50%] before:left-[155px] before:top-[55px] hover:before:translate-x-[-50px] hover:before:translate-y-[50px]">
+    <div className="w-fit h-max relative flex items-center justify-center before:w-[100px] before:h-[100px] before:content-[''] before:absolute before:bg-[rgb(144,161,255)] before:transition-all before:duration-[1s] before:rounded-[50%] before:left-[135px] before:top-[95px] hover:before:translate-x-[-50px] hover:before:translate-y-[50px]">
       <div className="relative w-72 h-96 flex flex-col items-center justify-between backdrop-blur-[30px] bg-[rgba(65,65,65,0.308)] border cursor-pointer px-2.5 py-5 rounded-[10px] border-solid border-[rgba(255,255,255,0.089)]">
         <p className="font-bold text-[0.9em] tracking-[1.2px] text-white text-center">
           {name}
