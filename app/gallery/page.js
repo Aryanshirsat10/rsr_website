059aba38f83585cafb10489c4e27057dfe41d89b
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-04-02 19:36:07 +0530

    Gallery Animation added

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,24 +1,16 @@
+"use client";
 import Navbar from "@/components/Navbar";
-import ImageComponent from "@/components/ImageComponent";
 
-import galleryImages from "@/constants/galleryImages";
+import { ParallaxScroll } from "@/components/ui/parallax-scroll";
 
-const galleryPage = async () => {
-  var photos = [];
-  galleryImages.forEach((element) => {
-    element.images.forEach((photo) => {
-      photos.push(photo);
-    });
-  });
+// import galleryImages from "@/constants/galleryImages";
+import galleryImagesAll from "@/constants/galleryImagesAll";
 
+const galleryPage = () => {
   return (
     <>
       <div> Carousel </div>
-      <div className="mt-16 pt-12 scroll-smooth p-10  columns-1 sm:columns-2 md:columns-3 lg:columns-4 space-y-4">
-        {photos.map((pic, index) => {
-          return <ImageComponent pic={pic} key={index} />;
-        })}
-      </div>
+      <ParallaxScroll images={galleryImagesAll} />
     </>
   );
 };
