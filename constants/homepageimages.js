ec762456ff069f7ebb90c0c73454924fe5109a5f
Aryan <aryanshirsat357@gmail.com>
2024-04-09 19:17:22 +0530

    created homepage

---
+++ constants/homepageimages.js
@@ -0,0 +1,19 @@
+const homeimg = [
+    {
+        title: "hero",
+        link: 'https://res.cloudinary.com/dma4lhef7/image/upload/v1712645891/website-images/edited%20photos/car1_emh5ui.jpg',
+    },
+    {
+        title: "journey",
+        link: "https://res.cloudinary.com/dma4lhef7/image/upload/v1712645892/website-images/edited%20photos/rsrcar_q74fvo.jpg",
+    },
+    {
+        title: "About",
+        link : "https://res.cloudinary.com/dma4lhef7/image/upload/v1712669313/website-images/edited%20photos/30.2_1683907057_mho9cr_1_bmaqa5.jpg"
+    },
+    {
+        title: "About1",
+        link : "https://res.cloudinary.com/dma4lhef7/image/upload/v1712645882/website-images/edited%20photos/astra_2_we9dpx.jpg"
+    }
+];
+export default homeimg;
